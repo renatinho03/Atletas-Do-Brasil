1ea2632b05203afd51f6abeb41c1d9ab2990e44a
Renato Henrique dos Santos Pereira <100329737+renatinho03@users.noreply.github.com>
2024-09-17 09:25:07 -0300

    dados.js

---
+++ js/dados.js
@@ -0,0 +1,92 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    categoria: "Skate",
+    descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil. Sua trajetória inspiradora mostra que a idade não é um limite para alcançar grandes feitos no esporte.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    facebook: "https://www.facebook.com/rayssalealsk8",
+    instagram: "https://www.instagram.com/rayssalealsk8",
+    foto: "rayssa_leal.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Beatriz Souza",
+    categoria: "Judô",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris. Com sua técnica impecável e determinação, ela conquistou a medalha de ouro, inspirando uma nova geração de atletas.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    facebook: null,
+    instagram: "https://www.instagram.com/_beatrizsouzaar",
+    foto: "beatriz_souza.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Rebeca Andrade",
+    categoria: "Ginastica",
+    descricao: "Rebeca Andrade, a joia da ginástica artística brasileira, conquistou o coração dos brasileiros com suas performances impecáveis e sua determinação inabalável. Com um currículo repleto de medalhas olímpicas e mundiais, ela se tornou um símbolo de superação e inspiração para jovens atletas.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    facebook: "https://www.facebook.com/rebeca.andrade.315428",
+    instagram: "https://www.instagram.com/rebecarandrade",
+    foto: "rebeca_andrade.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Martine Grael",
+    categoria: "Vela",
+    descricao: "Martine Grael é uma das maiores velejadoras brasileiras, com diversas conquistas e medalhas olímpicas. Sua parceria com Kahena Kunze é um dos maiores sucessos do esporte náutico brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Martine_Grael",
+    facebook: "https://www.facebook.com/graelkunze",
+    instagram: "https://www.instagram.com/martinegrael",
+    foto: "martine_grael.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Bruno Soares",
+    categoria: "Tenis",
+    descricao: "Bruno Soares é um tenista brasileiro que se destacou em duplas, conquistando títulos importantes no circuito mundial. Sua parceria com outros tenistas brasileiros e estrangeiros o levou ao topo do ranking mundial.",
+    link: "https://pt.wikipedia.org/wiki/Bruno_Soares",
+    facebook: "https://www.facebook.com/BrunoSoaresOficial",
+    instagram: "https://www.instagram.com/brunosoares82",
+    foto: "bruno_soares.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Ana Marcela Cunha",
+    categoria: "Natação",
+    descricao: "Ana Marcela Cunha é uma nadadora de maratonas aquáticas, com diversas medalhas em campeonatos mundiais e pan-americanos. Ela é considerada uma das maiores representantes do esporte no Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Ana_Marcela_Cunha",
+    facebook: "https://www.facebook.com/anamarcela92",
+    instagram: "https://www.instagram.com/anamarcela92",
+    foto: "ana_marcela_cunha.jpg",
+    curtidas: 0
+  },
+  {
+    titulo: "Neymar Júnior",
+    categoria: "Futebol",
+    descricao: "Neymar Jr., ou simplesmente Neymar, é um dos maiores jogadores de futebol da atualidade e um dos brasileiros mais famosos no mundo. Nascido em Mogi das Cruzes, São Paulo, em 5 de fevereiro de 1992, ele se destacou desde muito jovem por sua habilidade, dribles desconcertantes e finalizações precisas.",
+    link: "https://pt.wikipedia.org/wiki/Neymar#",
+    facebook: "https://www.facebook.com/neymarjr/?locale=pt_BR",
+    instagram: "https://www.instagram.com/neymarjr/",
+    foto: "neymar.jpg",
+    curtidas: 10
+  },
+  {
+    titulo: "Caio Bonfim",
+    categoria: "Marcha Atlética",
+    descricao: "Caio Oliveira de Sena Bonfim é um atleta brasileiro que compete nas marchas atléticas de 20 km e 35 km. Foi o primeiro brasileiro da história a ser medalhista olímpico nessa modalidade do atletismo, após ter alcançado a medalha de prata nos Jogos Olímpicos de Paris 2024, na marcha de 20 km.",
+    link: "https://pt.wikipedia.org/wiki/Caio_Bonfim",
+    facebook: "https://www.facebook.com/caiobonfims/?locale=pt_BR",
+    instagram: "https://www.instagram.com/caiobonfims/",
+    foto: "caioBonfim.jpg",
+    curtidas: 22
+  },
+  {
+    titulo: "Rosamaria Montibeller",
+    categoria: "Voleibol",
+    descricao: "Rosamaria Montibeller é uma jogadora de voleibol brasileira, vice-campeã olímpica, que atua nas posições de oposta e ponteira. Atualmente atua no Japão.",
+    link: "https://pt.wikipedia.org/wiki/Rosamaria_Montibeller",
+    facebook: "https://www.facebook.com/rosamaria.montibeller/?locale=pt_BR",
+    instagram: "https://www.instagram.com/rosamariaoficial/?img_index=1",
+    foto: "rosamaria.png",
+    curtidas: 16
+  }
+];
